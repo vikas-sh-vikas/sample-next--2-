dbaf3abcc5049e9d6f094486c086bc2badce4448
vikas-sh-udr <vikas.sh.udr@gmail.com>
2025-02-02 15:04:03 +0530

    eslint Update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,5 +12,12 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+module.exports = {
+  extends: ["next", "next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off", // Disable "Unexpected any"
+    "@typescript-eslint/no-unused-vars": "off", // Disable "Unused variables"
+  },
+};
 
 export default eslintConfig;
